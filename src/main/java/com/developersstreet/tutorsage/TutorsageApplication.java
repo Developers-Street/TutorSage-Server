2a4cd6d23cc9baad38f36bc5eb146e5f7648c9d5
Abhi088 <abhicoding8@gmail.com>
2021-12-30 23:30:18 +0530

    Initialized the project with spring boot

---
+++ src/main/java/com/developersstreet/tutorsage/TutorsageApplication.java
@@ -0,0 +1,13 @@
+package com.developersstreet.tutorsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorsageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developersstreet/tutorsage/TutorsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developersstreet.tutorsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
