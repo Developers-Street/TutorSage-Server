53e981738c1515621fd77af3618f7bc0e439724e
Abhi088 <abhicoding8@gmail.com>
2022-01-01 00:47:36 +0530

    removed command line runner

--- src/main/java/com/developersstreet/tutorsage/TutorsageApplication.java
+++ src/main/java/com/developersstreet/tutorsage/TutorsageApplication.java
@@ -24,26 +24,26 @@ public class TutorsageApplication {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Bean
-	CommandLineRunner run(UserService userService) {
-		return args -> {
-			userService.saveRole(new Role(null, "ROLE_USER"));
-			userService.saveRole(new Role(null, "ROLE_MANAGER"));
-			userService.saveRole(new Role(null, "ROLE_ADMIN"));
-			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-
-			userService.saveUser(new User(null, "John Cena", "john", "john@1234", new ArrayList<>()));
-			userService.saveUser(new User(null, "Randy Orton", "randy", "randy@1234", new ArrayList<>()));
-			userService.saveUser(new User(null, "Big Show", "show", "show@1234", new ArrayList<>()));
-			userService.saveUser(new User(null, "Roman Reigns", "roman", "roman@1234", new ArrayList<>()));
-
-			userService.addRoleToUser("john", "ROLE_USER");
-			userService.addRoleToUser("john", "ROLE_MANAGER");
-			userService.addRoleToUser("randy", "ROLE_MANAGER");
-			userService.addRoleToUser("show", "ROLE_ADMIN");
-			userService.addRoleToUser("roman", "ROLE_SUPER_ADMIN");
-			userService.addRoleToUser("roman", "ROLE_ADMIN");
-			userService.addRoleToUser("roman", "ROLE_USER");
-		};
-	}
+//	@Bean
+//	CommandLineRunner run(UserService userService) {
+//		return args -> {
+//			userService.saveRole(new Role(null, "ROLE_USER"));
+//			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+//			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+//			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+//
+//			userService.saveUser(new User(null, "John Cena", "john", "john@1234", new ArrayList<>()));
+//			userService.saveUser(new User(null, "Randy Orton", "randy", "randy@1234", new ArrayList<>()));
+//			userService.saveUser(new User(null, "Big Show", "show", "show@1234", new ArrayList<>()));
+//			userService.saveUser(new User(null, "Roman Reigns", "roman", "roman@1234", new ArrayList<>()));
+//
+//			userService.addRoleToUser("john", "ROLE_USER");
+//			userService.addRoleToUser("john", "ROLE_MANAGER");
+//			userService.addRoleToUser("randy", "ROLE_MANAGER");
+//			userService.addRoleToUser("show", "ROLE_ADMIN");
+//			userService.addRoleToUser("roman", "ROLE_SUPER_ADMIN");
+//			userService.addRoleToUser("roman", "ROLE_ADMIN");
+//			userService.addRoleToUser("roman", "ROLE_USER");
+//		};
+//	}
 }
