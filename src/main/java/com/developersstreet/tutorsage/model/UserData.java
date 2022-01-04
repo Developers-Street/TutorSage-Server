e0ab3c7ad3e007541ece74e2290060c16b88ef3f
Abhi088 <abhicoding8@gmail.com>
2022-01-04 13:57:34 +0530

    Bug fixed

--- src/main/java/com/developersstreet/tutorsage/model/UserData.java
+++ src/main/java/com/developersstreet/tutorsage/model/UserData.java
@@ -19,10 +19,10 @@ public class UserData {
     private String middle_name;
     private String last_name;
     private String gender;
-    private Number birth_day;
-    private Number birth_month;
-    private Number birth_year;
+    private String birth_day;
+    private String birth_month;
+    private String birth_year;
     private String profile_pic_url;
-    private Long phone_number;
+    private String phone_number;
     private Long user_id;
 }
